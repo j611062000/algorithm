058443a554633890011a0cdcd5e7a8bd8eba5064
SidLin <j611062000@gmail.com>
2019-01-04 14:35:34 +0800

    Q5

--- Advanced Algorithm/AAEX4/Q5_uva_714.py
+++ Advanced Algorithm/AAEX4/Q5_uva_714.py
@@ -1,8 +1,6 @@
 def min_max(data, k):
     lb, ub = max(data), sum(data)
-    mid = (lb + ub) // 2
-    c = 0
-    result = dict()
+    mid, result = (lb + ub) // 2, dict()
 
     while lb < ub:
         # if mid is the min_max, can we find out way that the number of the
@@ -11,11 +9,10 @@ def min_max(data, k):
         capacity_of_partition, accumulated_k = 0, 0
 
         for element in data:
-
             if (element + capacity_of_partition) <= mid and accumulated_k < k:
                 capacity_of_partition += element
 
-            elif accumulated_k < k:
+            elif accumulated_k + 1 < k:
                 accumulated_k += 1
                 capacity_of_partition = element
             else:
@@ -24,16 +21,18 @@ def min_max(data, k):
         if mid in result:
             break
 
-        result[mid] = k_is_to_small    
+        result[mid] = k_is_to_small
         if k_is_to_small:
+            lb = mid
             mid = (ub + lb) // 2
 
         else:
             ub = mid
             mid = (ub + lb) // 2
-    return min(result.keys())
+
+    return result
 
 if __name__ == '__main__':
-    data = [1, 2, 4, 1, 5, 2, 1,10,4,9,8]
+    data = [1,1,1,1,1]
     k = 3
     print(min_max(data, k))
